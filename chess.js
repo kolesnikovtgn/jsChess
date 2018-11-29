b32e7a88cda8cc0a945b7d21090effd590b89ad2
Kolesnikov Vladimir <lodoss.kolesnikov@gmail.com>
2018-11-29 12:21:22 +0300

    Remake programm with added coordinates with type 'a1' 'b2'

--- chess.js
+++ chess.js
@@ -5,12 +5,14 @@ let figureCoord = {
 
 let findCoord = {
   x: 7,
-  y: 2
+  y: 5
 };
+
 
 function f(figureCoord, findCoord) {
 
     let status;
+
 
     if(Math.abs(figureCoord.x - findCoord.x) == Math.abs(figureCoord.y - findCoord.y)) {
         status = true;
@@ -20,4 +22,26 @@ function f(figureCoord, findCoord) {
     return status;
 };
 
-console.log( f(figureCoord, findCoord) );
+
+
+
+let numb = [1, 2, 3, 4, 5, 6, 7, 8];
+let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+
+function f1(fig1, fig2) {
+    let index1;
+    let index2;
+    for(let i=0; i<alph.length; i++) {
+        if(fig1.charAt(0) == alph[i]) {
+            index1 = i+1;
+        }
+        if(fig2.charAt(0) == alph[i]) {
+            index2 = i+1;
+        }
+    }
+
+    return (Math.abs(index1-index2) == Math.abs(fig1.charAt(1)-fig2.charAt(1))) ? true : false;
+}
+
+console.log(f1('c7', 'e7'));
